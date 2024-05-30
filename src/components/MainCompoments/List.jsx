36cbf978a54dd7c92cbba4890c6bd5dbff9e32aa
dudnami <dudnami.gmail.com>
2024-05-30 19:31:34 +0900

    친구추가 모달창 수정완

--- src/components/MainCompoments/List.jsx
+++ src/components/MainCompoments/List.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from './Search';
 import Profile from "../../assets/pro.jpg";
+import Modal from '../F_AddModal/F_Add'; // 경로를 정확히 설정하세요.
 import "./style.css";
-import FA from "../../assets/친구추가.svg"
+import FA from "../../assets/친구추가.svg";
 
 const List = ({ onUserChatClick }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleUserChatClick = () => {
     onUserChatClick(); // 클릭 시 onUserChatClick 함수 호출
+  };
+
+  const openModal = () => {
+    console.log("Opening modal");
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    console.log("Closing modal");
+    setIsModalOpen(false);
   };
 
   return (
     <div className='list'>
       <div className='navbar'>
         <span className='f-list'>친구 목록</span>
-        <img src={FA} className='f-a' />
+        <img src={FA} className='f-a' onClick={openModal} alt="친구추가" />
       </div>
       <Search />
       <div className='chats'>
@@ -36,10 +48,11 @@ const List = ({ onUserChatClick }) => {
           <img src={Profile} alt="Profile"/>
           <div className="userChatInfo">
             <span>소프트웨어융기</span>
-            <p>종합설게 소프트웨어융기 팀</p>
+            <p>종합설계 소프트웨어융기 팀</p>
           </div>
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 };
