b7eb7284de34086ec00ba5b251af3c6b4b9e05cd
feuzw <feuzw21@gmail.com>
2024-06-03 03:22:35 +0900

    프로필이름연동

--- src/components/MainCompoments/My.jsx
+++ src/components/MainCompoments/My.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect , useState} from 'react'
 import Profile from "../../assets/profile.png"
 import 편집 from "../../assets/편집.svg"
 import 나와 from "../../assets/나와.svg"
+import {useCookieManager} from "../../customHook/useCookieManager";
 
 const My = () => {
+  const [userNickName, setUserNickName] = useState('');
+  const {getCookies} = useCookieManager();
+  const { accessToken, refreshToken } = getCookies();
+
+  useEffect(() => {
+    setUserNickName('로그인을 해주세요!');
+        
+    if (accessToken && refreshToken) {
+            const localAccessToken = getCookies().accessToken;
+        fetch('http://localhost:8080/auth/user', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${localAccessToken}` 
+            }
+          })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json(); // JSON 형식으로 응답 데이터를 파싱
+          })
+          .then(data => {
+            // 응답 데이터(data)를 처리
+            setUserNickName(data.resultData);
+            console.log(data);
+          })
+          .catch(error => {
+          });
+    }
+    
+}, [getCookies.accessToken, getCookies.refreshToken]);
+
+
+
+
+
+
   return (
   <div className='my'>
       <div className='navbar'>
@@ -12,7 +51,7 @@ const My = () => {
 
   <div className="my-profile">
     <img src={Profile} alt="" />
-    <span>종설</span>
+    <span>{userNickName}</span>
   </div>
   
   <div className="mee">
